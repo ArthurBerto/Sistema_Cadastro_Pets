918dad04a35d6cd2d5f0f5d096eee31d2e7daebf
Arthur Berto <arthur.berto.ferreira@gmail.com>
2025-03-12 12:16:56 -0300

    Exibe Menu

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ const data = fs.readFileSync("../data/formulario.txt", {
   encoding: "utf-8",
   flag: "r",
 });
-console.log(data);
+
+const operacao = () => {
+  const menu = `1. Cadastrar um novo pet
+2. Alterar os dados do pet cadastrado
+3. Deletar um pet cadastrado
+4. Listar todos os pets cadastrados
+5. Listar pets por algum critério (idade, nome, raça)
+6. Sair`;
+
+  console.log(menu);
+};
+
+operacao();
